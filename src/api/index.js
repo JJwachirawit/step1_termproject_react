e3827b0ee1b59be3fecdb9776bcc93a02494cfb0
wachirawit <wachirawit_mo66@live.rmutl.ac.th>
2024-04-07 17:05:19 +0700

    Add: Step1 for termproject react

---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+export async function login({ email, password }) {
+  return await fetch("http://api-user66027.se-rmutl.net/api/auth/login", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => {
+      // If request is not successful, display error message
+      if (!response.ok) {
+        throw new Error("HTTP status " + response.status);
+      }
+
+      return response.json();
+    })
+    .catch((err) => {
+      console.error("Error during login:", err);
+      // คุณสามารถทำการ handle ข้อผิดพลาดที่นี่ได้ เช่น แสดงข้อความแจ้งเตือนหรือทำการล็อกอินใหม่
+    });
+}
+
+export async function searchArtworks({ keyword }) {
+  return await fetch(`http://api-user66027.se-rmutl.net/api/homepage/getArtworks/${keyword}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => {
+      // If request is not successful, display error message
+      if (!response.ok) {
+        throw new Error("HTTP status " + response.status);
+      }
+
+      return response.json();
+    })
+    .catch((err) => {
+      console.error("Error during artwork search:", err);
+      // คุณสามารถทำการ handle ข้อผิดพลาดที่นี่ได้ เช่น แสดงข้อความแจ้งเตือนหรือทำการลองค้นหาใหม่
+    });
+}
